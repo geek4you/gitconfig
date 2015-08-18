8f771834fda75d52809752a0fb4b08ad0047576d
Geek 4 You <chanumolu@yahoo.com>
2015-08-18 01:35:13 -0700

    my git congifs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gitconfig
 My Git configurations.
+
+## Install
+
+1. Run install script:
+  * `./install.sh`
+
+## Used by
+* [geek4you/dotfiles](https://github.com/geek4you/dotfiles)
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+ABS=$($READLINK -f $0)
+BASE=$(dirname $ABS)
+
+git submodule update --init
+
+source ./template.sh/template.sh
+
+
+## variables
+
+GITHUB_USER="geek4you"
+GIT_NAME="Geek 4 You"
+GIT_EMAIL="chanumolu@yahoo.com"
+read -p "Git email (default: $GIT_EMAIL): " _GIT_EMAIL
+if [ ! -z $_GIT_EMAIL ]; then
+	GIT_EMAIL=$_GIT_EMAIL
+fi
+echo "GIT_EMAIL=${GIT_EMAIL}"
+read -p "GitHub token: " _GITHUB_TOKEN
+if [ ! -z $_GITHUB_TOKEN ]; then
+	GITHUB_TOKEN=$_GITHUB_TOKEN
+fi
+echo "GITHUB_TOKEN=${GITHUB_TOKEN}"
+read -p "GitHub password: " _GITHUB_PASSWORD
+if [ ! -z $_GITHUB_PASSWORD ]; then
+	GITHUB_PASSWORD=$_GITHUB_PASSWORD
+fi
+echo "GITHUB_PASSWORD=${GITHUB_PASSWORD}"
+
+
+## run
+
+cd "$BASE"
+
+ln -sf "$BASE/gitconfig" ~/.gitconfig
+template "gitconfig-github" > ~/.gitconfig-github
+template "gitconfig-user" > ~/.gitconfig-user
+
